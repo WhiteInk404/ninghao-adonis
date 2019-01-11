b3e9abf8d5e254d30f613f60c50c096051e9dd30
SherDavincl <johnnypei@outlook.com>
2019-01-11 09:21:11 +0800

    Request

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,9 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route
+
+  .on('/').render('welcome')
+
+Route
+  .get('/posts', ({ request }) => request.get() )
